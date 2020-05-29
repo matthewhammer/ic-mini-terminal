653d407b6c8b59d8cc206f1a9d22e4c2bf936205
Matthew Hammer <matthew@dfinity.org>
2020-05-29 14:52:45 -0600

    cleanup

--- moc-build.sh
+++ moc-build.sh
@@ -1,1 +1,3 @@
-moc `./vessel sources` -c test/Main.mo
+# Use this in place of `dfx build` if you have issues with dfx+vessel
+moc `./vessel sources` -c test/Main.mo && \
+    cp Main.wasm canisters/test/Main.wasm

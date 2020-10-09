68cf72e761806d43a847eb4de94fccc396dbce53
Matthew Hammer <matthew@dfinity.org>
2020-10-09 11:18:15 -0600

    wtf

--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+echo dfx start &&\
 dfx -vv start --background &&\
+echo canister create &&\
 dfx canister create textEdit &&\
+echo build textEdit &&\
 dfx build textEdit &&\
+echo canister install &&\
 dfx canister install textEdit &&\
+echo canister call windowSizeChange &&\
 dfx canister call windowSizeChange 'record {width=100; height=100; }' &&\
+echo canister call queryKeyDown &&\
 dfx canister call queryKeyDown 'vec { }' &&\
- #cargo run --release -- connect 127.0.0.1:8000 `dfx canister id textEdit`
 echo Tests done. Success.
 

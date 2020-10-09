d2b3b668309a7da4ceda74f2dd69aac96e3b769d
Matthew Hammer <matthew@dfinity.org>
2020-10-09 11:31:14 -0600

    ping

--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 echo dfx start &&\
 dfx -vv start --background &&\
+\
 echo canister create &&\
 dfx canister create textEdit &&\
+\
 echo build textEdit &&\
 dfx build textEdit &&\
+\
 echo canister install &&\
 dfx canister install textEdit &&\
+\
 echo canister call windowSizeChange &&\
 dfx canister call windowSizeChange 'record {width=100; height=100; }' &&\
+\
 echo canister call queryKeyDown &&\
 dfx canister call queryKeyDown 'vec { }' &&\
+\
 echo Tests done. Success.
 

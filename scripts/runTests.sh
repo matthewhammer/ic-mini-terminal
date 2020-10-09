739964524a3e38d41b549a8a6c631eb80bfa8d1b
Matthew Hammer <matthew@dfinity.org>
2020-10-09 11:11:24 -0600

    bash script semantics

--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-dfx -vv start --clean --background
-dfx canister create textEdit
-dfx build textEdit
-dfx canister install textEdit
-dfx canister call windowSizeChange 'record {width=100; height=100; }'
-dfx canister call queryKeyDown 'vec { }'
+dfx -vv start --clean --background &&\
+dfx canister create textEdit &&\
+dfx build textEdit &&\
+dfx canister install textEdit &&\
+dfx canister call windowSizeChange 'record {width=100; height=100; }' &&\
+dfx canister call queryKeyDown 'vec { }' &&\
  #cargo run --release -- connect 127.0.0.1:8000 `dfx canister id textEdit`
+echo Tests done. Success.
 

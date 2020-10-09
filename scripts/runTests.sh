21542563ae9606220b023edec0307a2e221d9943
Matthew Hammer <matthew@dfinity.org>
2020-10-09 11:50:41 -0600

    fix missing dfx call param

--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -11,11 +11,11 @@ dfx build textEdit &&\
 echo canister install &&\
 dfx canister install textEdit &&\
 \
-echo canister call windowSizeChange &&\
-dfx canister call windowSizeChange 'record {width=100; height=100; }' &&\
+echo canister call textEdit windowSizeChange &&\
+dfx canister call textEdit windowSizeChange 'record {width=100; height=100; }' &&\
 \
-echo canister call queryKeyDown &&\
-dfx canister call queryKeyDown 'vec { }' &&\
+echo canister call textEdit queryKeyDown &&\
+dfx canister call textEdit queryKeyDown 'vec { }' &&\
 \
 echo Tests done. Success.
 

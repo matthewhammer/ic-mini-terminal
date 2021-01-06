13ce781635dda0f049f94d18f80aa39a82e27406
Matthew Hammer <matthew@dfinity.org>
2021-01-06 15:49:59 -0700

    more cleanup

--- scripts/bobConnects.sh
+++
@@ -1,6 +0,0 @@
-
-#!/bin/bash
-VERSION=`cat .DFX_VERSION`
-export PATH=~/.cache/dfinity/versions/$VERSION:`pwd`:$PATH
-echo "Bob connects to Alice's text editor canister"
-cargo run --release -- connect 127.0.0.1:8000 `dfx canister id textEdit` --user '("Bob", (200, 100, 255))'
--- scripts/dfx-start-background.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-VERSION=`cat .DFX_VERSION`
-export PATH=~/.cache/dfinity/versions/$VERSION:`pwd`:$PATH
-dfx -vv start --clean --background

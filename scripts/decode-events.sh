73ce845dbba8f79100dad0c150248bb053281199
Matthew Hammer <matthew@dfinity.org>
2021-01-11 12:09:29 -0700

    remove local Motoko code and use MirrorGarden for demo(s)

---
+++ scripts/decode-events.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ -f "${1}" ]; then
+  didc decode `cat ${1}` -d service.did -t '(vec EventInfo)'
+else
+    echo "Error within ${0}: File not found:"
+    echo " - Could not find event file ${1}"
+    echo
+    echo "usage: ${0} <icmt-events-filename>"
+fi

56e53f2c01250d61c611f31ce30f8daf8e1e2b1f
Matthew Hammer <matthew@dfinity.org>
2021-01-22 08:27:46 -0700

    Fix: Dump correct events (#64)
    * fixups for demos
    
    * script polish, with help

--- scripts/MirrorGarden-demo.sh
+++ scripts/MirrorGarden-demo.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 VERSION=`cat .DFX_VERSION`
 export PATH=~/.cache/dfinity/versions/$VERSION:`pwd`:$PATH
-echo
-echo Starting: Mirror Garden demo...using DFINITY SDK replica
-echo
-dfx stop &&\
-dfx start --background --clean &&\
-dfx canister create MirrorGarden &&\
-dfx build MirrorGarden &&\
-dfx canister install MirrorGarden
+case ${1} in
+  "-h" | "--help" | "help")
+  echo "usage examples:"
+  echo "  - Start Mirror Garden, but no mini terminals: ${0}"
+  echo "  - Start Mirror Garden, and one mini terminal: ${0} 1"
+  echo "  -       Same scenario, but with some logging: ${0} 1 -L"
+  echo "  - Start Mirror Garden, and two mini terminals: ${0} 2"
+  echo "  -        Same scenario, but with some logging: ${0} 2 -L"
+  echo
+  exit 0
+  ;;
+  *)
+  echo
+  echo Starting: Mirror Garden demo...using DFINITY SDK replica
+  echo
+  dfx stop &&\
+  dfx start --background --clean &&\
+  dfx canister create MirrorGarden &&\
+  dfx build MirrorGarden &&\
+  dfx canister install MirrorGarden
+  ;;
+esac
 echo
 echo Mirror Garden canister:
 echo   `dfx canister id MirrorGarden`
@@ -19,11 +33,15 @@ echo
 echo   ic-mt connect 127.0.0.1:8000 `dfx canister id MirrorGarden` --user '("User", (100, 200, 200))'
 echo
 case ${1} in
+  "-h" | "--help" | "help")
+    echo "unreachable!"
+    exit -1
+    ;;
   "" | "0")
     echo No initial terminals requested.  All done.
     ;;
   "1")
-    ic-mt ${2} connect 127.0.0.1:8000 `dfx canister id MirrorGarden` --user '("Alice", (100, 200, 200))'
+    ic-mt ${2} connect 127.0.0.1:8000 `dfx canister id MirrorGarden` --user '("Alice", (100, 200, 200))' &
     echo
     echo Started one live terminal for Alice.
     echo
